ec492a2eac8ffb8d47d970ded91e7d4c7b2e1b85
bromba <bromba1@tlen.pl>
2017-06-08 12:24:04 +0200

    .gitignore - node folder added

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+//tytul pliku
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+/* console.log pozwala na wykonanie polecenia z poziomu konsoli
+*/
+/* jeszcze jeden komentarz blkowy*/
+// w przeciwienstwie do komentarza liniowego 
